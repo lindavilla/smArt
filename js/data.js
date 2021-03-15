3817eebb4cbd031a699bc2521ae204fc5d0484fb
Linda Villa <linda.villa@alea.com>
2021-03-15 22:48:39 +0100

    Create Files

---
+++ js/data.js
@@ -0,0 +1,202 @@
+let artWorks = [
+
+    {
+    "id" : 1,
+    "title": "Mona Lisa",
+    "painter": "Leonardo Da Vinci",
+    "year": 1503, 
+    "era": "Renaissance",
+    "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+    },
+ 
+    {
+     "id" : 2,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+    {
+     "id" : 3,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 4,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 5,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 6,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 7,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 8,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 9,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 10,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 11,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 12,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 13,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 14,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 15,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 16,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 17,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 18,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 19,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    },
+ 
+    {
+     "id" : 20,
+     "title": "Mona Lisa",
+     "painter": "Leonardo Da Vinci",
+     "year": 1503, 
+     "era": "Renaissance",
+     "image": "https://images.app.goo.gl/8g7e6PH4Gu8kZriq6"
+ 
+    }
+ 
+ ]
+ 
