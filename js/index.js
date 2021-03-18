d9b504799df78dccb3a2185ee4691884aabb480e
Linda Villa <linda.villa@alea.com>
2021-03-18 17:53:34 +0100

    Style Image Divs

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // Temporary for the development of the game, to remove when the game is finished
-//ocument.getElementsByClassName("startPage")[0].style.display = "none";
+document.getElementsByClassName("startPage")[0].style.display = "none";
+document.getElementsByClassName("gamePage")[0].style.display = "flex";
 
 
 const startPage = document.getElementById("startPage")[0];   
@@ -53,22 +54,3 @@ function clickStart(){
     showGameScreen();
   };
 
-  function buildGameScreen() {
-    gameScreen = buildDom(`
-          <div id="viking-army" class="army">
-              <h2>Vikings</h2>
-              <ul></ul>
-          </div>
-          <div id="saxon-army" class="army">
-              <h2>Saxons</h2>
-              <ul></ul>
-          </div>
-          <div class="status-message">
-              Click a button to start fighting!
-          </div>
-          <div class="attacks">
-              <button id="viking-attack" class="button">Viking Attack</button>
-              <button id="saxon-attack" class="button">Saxon Attack</button>
-          </div> 
-    `);
-  }*/
