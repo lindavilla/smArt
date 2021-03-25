400bf2f39cb9970430cb596ae958e3fa2ce15ae6
Linda Villa <linda.villa@alea.com>
2021-03-25 19:05:57 +0100

    Update painting.js

--- js/painting.js
+++ js/painting.js
@@ -8,8 +8,9 @@ class Painting {
     }
 
     getImage(){
-        return "https://raw.githubusercontent.com/lindavilla/smArt/MVP/images/"+this.image;
-        //return "../images/"+this.image;
+        if(location.host == "127.0.0.1:5500") return "../images/"+this.image;
+        else return "https://raw.githubusercontent.com/lindavilla/smArt/MVP/images/"+this.image;
+
     }
 
     getEra(){
