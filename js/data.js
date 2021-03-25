7677e91e72e8a658ba48be0a339a9c64c27a8dbc
Linda Villa <linda.villa@alea.com>
2021-03-25 17:20:07 +0100

    Paintings Added

--- js/data.js
+++ js/data.js
@@ -1,24 +1,36 @@
 let artWorks = [
    
-    
-    
-    new Painting("Gsdffsd", "Leonardo Da Vinci", "1503", "Realism", "NeoclassicismFrance.jpg"),
-    new Painting("Gsdffsd", "Leonardo Da Vinci", "1503", "Realism", "NeoclassicismFrance1.jpg"),
-    new Painting("Gsdffsd", "Leonardo Da Vinci", "1503", "Realism", "RococoEngland.jpg"),
-    new Painting("Gsdffsd", "Leonardo Da Vinci", "1503", "Realism", "RococoFrance.jpg"),
-    new Painting("Gsdffsd", "Leonardo Da Vinci", "1503", "Realism", "RococoFrance1.jpg"),
-
-    
     new Painting("Three Women at the Tomb c.1346", "Ferrer Bassa", "1346", "Medieval", "MedievalCatalan.jpg"),
     new Painting("The Consecration of St Marcus c.1350", "Arnau Bassa", "1350", "Medieval", "MedievalCatalan1.jpg"),
-    new Painting("Assumption of the Virgin c.1550", "Tintoretto", "1550", "Renaissance", "RenaissanceItaly.jpg"),
+    new Painting("Mona Lisa c.1503", "Leonardo da Vinci", "1503", "Renaissance", "RenaissanceItaly.jpg"),
     new Painting("The Creation of Man c.1683", "Luca Giordano", "1683", "Renaissance", "RenaissanceItaly2.jpg"),
     new Painting("The Milkmaid c.1658", "Johannes Vermeer", "1658", "Baroque", "baroque.jpg"),
     new Painting("Girl with a Pearl Earring c.1665", "Johannes Vermeer", "1665", "Baroque", "Baroque1.jpg"),
+    new Painting("The Blue Boy c.1770", "Thomas Gainsborough", "1770", "Rococo", "RococoEngland.jpg"),
+    new Painting("The Swing c.1767", "Jean-Honoré Fragonard", "1767", "Rococo", "RococoFrance1.jpg"),
+    new Painting("Napoleon I On His Imperial Throne c.1806", "Jean Auguste Dominique Ingres", "1806", "Neoclassicism", "NeoclassicismFrance1.jpg"),
+    new Painting("The Coronation of Napoleon c.1807", "Jacques-Louis David", "1807", "Neoclassicism", "NeoclassicismFrance.jpg"),
     new Painting("The Wanderer above the Mists c.1817", "Caspar Friedrich", "1817", "Romanticism", "RomanticismGerman.jpg"),
     new Painting("Saturn Devouring One of his Children c.1819", "Francisco Goya", "1819", "Romanticism", "RomanticismSpain.jpg"),
     new Painting("The Desperate man c.1844", "Gustave Courbet", "1844", "Realism", "RealismFrench.jpg"),
     new Painting("Olympia c.1863", "Edouard Manet", "1863", "Realism", "RealismFrance.jpg"),
     new Painting("Fisherman c.1874", "Adolph Felix Cals", "1874", "Impressionism", "ImpressionismFrench.jpg"),
     new Painting("Sunlight c.1909", "Frank Weston Benson", "1909", "Impressionism", "ImpressionismAmerican.jpg"),
+    new Painting("A Sunday Afternoon On The Island Of La Grande-Jatte c.1884", "Georges Seurat", "1884", "Post-Impressionism", "postImpressionism.jpg"),
+    new Painting("The Starry Night c.1889", "Vincent Van Gogh", "1889", "Post-Impressionism", "postImpressionism1.jpg"),
+    new Painting("La Trapistine c.1897", "Alphonse Mucha", "1897", "Art Nouveau", "artNouveau.jpg"),
+    new Painting("Monaco Monte Carlo c.1897", "Alphonse Mucha", "1897", "Art Nouveau", "artNouveau1.jpg"),
+    new Painting("Chop Suey c.1939", "Edward Hopper", "1939", "New Realism", "newRealism1.jpg"),
+    new Painting("Nighthawks c.1942", "Edward Hopper", "1942", "New Realism", "newRealism.jpg"),
+    new Painting("Mao c.1972", "Andy Warhol", "1972", "Pop Art", "popArt.jpg"),
+    new Painting("Shot Marilyns c.1964", "Andy Warhol", "1964", "popArt", "popArt1.jpg"),
+    new Painting("The Great Wave Off Kanagawa c.1831", "Hokusai", "1831", "Ukiyo-e", "ukiyo-e.jpg"),
+    new Painting("Two Beauties with Bamboo c.1795", "Utamaro", "1795", "Ukiyo-e", "ukiyo-e1.jpg"),
+    new Painting("Emperor Jahangir visiting the ascetic Jadrup", "Govardhan", "1616", "Mughal", "Mughal.jpg"),
+    new Painting("Babur Receives a Courtier by Farrukh Beg", "N/A", "1580", "Mughal", "Mughal1.jpg"),
+    new Painting("Bharat Mata c.1905", "Abanindranath Tagore", "1905", "Bengal School of Art", "bengalArt.jpg"),
+    new Painting("Asoka's Queen c.1910", "Abanindranath Tagore", "1910", "Bengal School of Art", "bengalArt1.jpg"),
+    new Painting("Lascaux Drawings c.30,000BC", "", "", "Stone Age", "stoneAge.jpg"),
+    new Painting("Lascaux Drawings c.30,000BC", "", "", "Stone Age", "stoneAge1.jpg"),
+
 ]
